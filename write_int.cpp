c2aa541e8d8300b679386166338441554aa9f6fb
Edwin Richbald A. Salinas <edsalinas7b@yahoo.com>
2017-01-07 17:12:13 +0800

    Create write_int.cpp

---
+++ write_int.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+main(){
+    FILE *myFile;
+    myFile = fopen("sample.txt", "r");
+
+    //read file into array
+    int i, sum = 0;
+    int numberArray[5];
+
+    for(int i=0; i<5; i++){
+        fscanf(myFile, "%d", &numberArray[i]);
+        sum+=numberArray[i];
+    }
+    fclose(myFile);
+    myFile = fopen("sample.txt", "w");
+    for(int i=0; i<5; i++){
+        fprintf(myFile, "%d", numberArray[i]);
+    }
+    cout << sum;
+
+}
