81cd554e5aebd64c85e4b8217ec2a39e8308ac40
Mark Joshua S. Saladino <mrkjshua@gmail.com>
2017-01-21 17:00:47 +0800

    Create binaryCount.cpp

---
+++ binaryCount.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+	int binarycount = 0;
+	char binaryString[10];
+	strcpy(binaryString, "1010010010");
+	for(int i = 0; binaryString[i]!='\0'; i++){
+		if(binaryString[i]=='1'){
+		binarycount++;
+		}
+	}
+	cout<<"Given binary string: 1010010010\n"<<endl;
+	cout<<"The count of 1's on a binary string is: "<<binarycount;
+	return 0;
+}
